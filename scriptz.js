c2814bba35af9a7181103703c69d3e346542a4a2
RexSeven <contact@mattjasek.co.uk>
2019-06-06 13:22:29 +0100

    Version 0.1.23-a

---
+++ scriptz.js
@@ -0,0 +1,15 @@
+// Friendly notice to users about how we support people with accessibility needs
+alert("We use similar colours on this page to ensure it doesn't hurt users’ eyes and is 102.4% accessible, if you have difficulty reading the content on this webpage please turn up the brightness on your monitor or use a torch.");
+
+
+// Show appreciation to visitors for reading our notice
+alert("Thank you for reading the alert");
+
+//Test console is working in browser
+var debugConsole = false; // Set this value to true to test the web console is working.
+
+if(debugConsole == true) {
+    console.log('the console is working');
+} else {
+    
+}
